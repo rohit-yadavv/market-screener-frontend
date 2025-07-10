6c520a7fb889736d7cd5ee3e10e2243549498309
Rohit Yadav <rk4740779@gmail.com>
2025-07-10 13:03:08 -0400

    udpated login issue

--- src/App.jsx
+++ src/App.jsx
@@ -3,33 +3,32 @@ import Login from "./pages/Login.jsx";
 import Dashboard from "./pages/Dashboard.jsx";
 import { Navigate } from "react-router-dom";
 import { useAuth } from "./hooks/useAuth.js";
+import { useEffect } from "react";
 
-function ProtectedRoute({ children }) {
-  const { loading, isAuthenticated } = useAuth();
+function ProtectedRoute({ children, loading, isAuthenticated }) {
+  useEffect(() => {
+    if ("serviceWorker" in navigator) {
+      navigator.serviceWorker.register("/sw.js").then(() => {
+        console.log("✅ Service Worker registered");
+      });
+    }
+  }, []);
 
-  if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.register("/sw.js").then(() => {
-      console.log("✅ Service Worker registered");
-    });
-  }
-
-  if (loading) {
-    return <div className="p-6">Checking authentication...</div>;
-  }
-  if (!isAuthenticated) {
-    return <Navigate to="/login" replace />;
-  }
+  if (loading) return <div className="p-6">Checking authentication...</div>;
+  if (!isAuthenticated) return <Navigate to="/login" replace />;
   return children;
 }
 
 export default function App() {
+  const { loading, isAuthenticated } = useAuth();
+
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
       <Route
         path="/"
         element={
-          <ProtectedRoute>
+          <ProtectedRoute loading={loading} isAuthenticated={isAuthenticated}>
             <Dashboard />
           </ProtectedRoute>
         }
