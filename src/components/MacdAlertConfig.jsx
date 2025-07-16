34c98285aa501a735b109cb372fc30ec86f90f26
Rohit Yadav <rk4740779@gmail.com>
2025-07-16 20:14:33 +0530

    bug fix

--- src/components/MacdAlertConfig.jsx
+++ src/components/MacdAlertConfig.jsx
@@ -270,7 +270,7 @@ export default function StockSelector() {
               onClick={handleReset}
               disabled={saving}
             >
-              {saving ? "Resetting..." : "Reset"}
+              Reset
             </Button>
           </CardFooter>
         </Card>
