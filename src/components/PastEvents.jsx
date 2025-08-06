de021485ab54864c8a03c999b753086c51ac353f
Rohit Yadav <rk4740779@gmail.com>
2025-08-06 14:56:13 -0400

    updated count for alert card

--- src/components/PastEvents.jsx
+++ src/components/PastEvents.jsx
@@ -53,7 +53,7 @@ export function AlertCard({ event, type }) {
             </Badge>
             <Badge className="flex items-center gap-1 bg-yellow-100 text-yellow-800 text-[11px] font-medium px-2.5 py-0.5 border border-yellow-300 rounded-sm min-w-fit">
               <Clock className="w-3 h-3" />
-              Count: {event.streak}
+              Count: {event.count}
             </Badge>
           </>
         )}
