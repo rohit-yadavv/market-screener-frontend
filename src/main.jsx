03dd9580ef625a3370d2da2235d4c5293a4ccdb5
Rohit Yadav <rk4740779@gmail.com>
2025-08-26 15:10:52 -0400

    updated hashrouter to replace react router

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext.jsx";
 import "./index.css";
 import { Toaster } from "sonner";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <AuthProvider>
         <Toaster position="top-center" richColors />
         <App />
       </AuthProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
